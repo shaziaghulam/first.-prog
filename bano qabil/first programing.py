33d9286708685fa4d393d52f6dc78ab889f29ea7
shaziaghulam <squeensqueen23@gmail.com>
2024-10-30 17:05:05 +0500

    Add files via upload

---
+++ bano qabil/first programing.py
@@ -0,0 +1,33 @@
+a=20
+b=10
+c=15
+sum=a+b+c
+difference=a-b-c
+product=a*b*c
+average=a+b+c/3
+print("sum",sum)
+print("difference",difference)
+print("product",product)
+print("average",average)
+
+
+base=5
+height=4
+def triangle_area(base,height):
+    return 0.5*base*height
+print(triangle_area(base,height))
+
+side=2*2
+def square_area(side):
+    return side**2
+print(square_area(side))
+
+import math
+radius=7
+circle_area=math.pi*radius**2
+print(circle_area)
+
+length=6
+width=4
+rectangle_area=length*width
+print(rectangle_area)
